39eb61c44bf94d315a9f589ba037ec5e6d8f6ddc
KARTHIK EDITZ <93807720+karthikeditz@users.noreply.github.com>
2023-06-17 16:08:49 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,12 +65,13 @@
   const countDownDate = function() {
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
+    let years = "";
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+    countdown.innerHTML = output.replace('1', years).replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
   }
   countDownDate();
   setInterval(countDownDate, 1000);
